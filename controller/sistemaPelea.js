3eac7127d6e68ffff90606e843c4277cd359efc2
Juan Pablo <jpccdaw@@gmail.com>
2022-12-16 12:07:41 +0000

    problema con comienza la pelea

--- controller/sistemaPelea.js
+++ controller/sistemaPelea.js
@@ -8,42 +8,52 @@ forNombre.addEventListener("input", () => {
   const nombreJugador = document.getElementById("nombreJugador");
   localStorage.setItem("Nombre", nombreJugador.value);
 });
-const pokemonRandom = new Pokemon(n);
+const pokemonAliado = new Pokemon(Math.floor(Math.random() * 905 + 1));
 export let player = new Jugador(localStorage.getItem("Nombre"));
 export let enemy = new Jugador("Pepe el dominguero");
 //Se crea el primer pokemon aleatorio -->
 const cargarDatoPokemon = () => {
-  // console.log(pokemonRandom.nombre);
+  // console.log(pokemonAliado.nombre);
+
   creaPokemon();
+  grupoEnemigo(Math.floor(Math.random() * 905 + 1));
+  grupoEnemigo(Math.floor(Math.random() * 905 + 1));
+  grupoEnemigo(Math.floor(Math.random() * 905 + 1));
+  printaPelea()
 };
+
+
+const pintaDatos = () => {
+  
+};
+
 
 setTimeout(() => {
   cargarDatoPokemon();
 }, 1000);
 
+
+
 export function creaPokemon() {
   // console.log(data);
   //si ya existe un pokemon en tu array se mete en el array enemigo
-  if (player.tusPokemon.length == 0) {
-    player.tusPokemon.push(pokemonRandom);
-    let sprite1 = document.getElementById("sprite1");
-    let sprite2 = document.getElementById("sprite2");
-    let pokename = document.getElementById("pokeName");
-    pokename.innerHTML = pokemonRandom.nombre;
-    sprite1.src = pokemonRandom.spriteDelantero;
-    sprite2.src = pokemonRandom.spriteTrasero;
-  } else {
-    enemy.tusPokemon.push(pokemonRandom);
-  }
+
+  player.tusPokemon.push(pokemonAliado);
+  let sprite1 = document.getElementById("sprite1");
+  let sprite2 = document.getElementById("sprite2");
+  let pokename = document.getElementById("pokeName");
+  pokename.innerHTML = pokemonAliado.nombre;
+  sprite1.src = pokemonAliado.spriteDelantero;
+  sprite2.src = pokemonAliado.spriteTrasero;
 }
-// console.log("tus pokemon", player.tusPokemon);
-// console.log("pokemon enemigos", enemy.tusPokemon);
+console.log("tus pokemon", player.tusPokemon);
+console.log("pokemon enemigos", enemy.tusPokemon);
 
 let botonPelear = document.getElementById("botonPelear");
 
-function grupoEnemigo() {
-  for (let i = 0; i < 3; i++)
-    consultaEnemigo(Math.floor(Math.random() * 905 + 1));
+function grupoEnemigo(n) {
+  let pokemonEnemigo = new Pokemon(n);
+  enemy.tusPokemon.push(pokemonEnemigo);
 }
 //quita la prepelea e imprime el sistema de combate
 let pelea = document.getElementById("pelea");
@@ -60,7 +70,7 @@ botonPelear.addEventListener("click", ocultarPrepelea);
 ///COMIENZA LA PELEA
 const printaPelea = () => {
   let tuPokemon = document.getElementById("tuPokemon");
-  // tuPokemon.src = player.tusPokemon.spriteTrasero;
+  console.log(player.tusPokemon[0].nombre);
+  tuPokemon.innerHTML = player.tusPokemon[0].nombre;
 };
 
-printaPelea();
